87b712f81f485d63b422f0b05ee4caa4a1060d0c
“savvas-paragkamian” <s.paragamian@gmail.com>
2023-06-01 16:37:47 +0300

    xml element tree extraction of elements, need to format correctly

--- scripts/ena_xml_to_csv.py
+++ scripts/ena_xml_to_csv.py
@@ -14,6 +14,7 @@
 import xml.etree.ElementTree as ET
 import csv
 import os,sys
+import pandas as pd
 
 # Directory containing the XML files
 xml_dir = 'ena_samples_attr/'
@@ -28,22 +29,59 @@ data = []
 for filename in os.listdir(xml_dir):
     if filename.endswith('.xml'):
         # Parse the XML file
+        
         tree = ET.parse(os.path.join(xml_dir, filename))
         root = tree.getroot()
+        #pd_df = pd.read_xml(os.path.join(xml_dir, filename))
+        #print(pd_df)
+
         print(filename)
-        sys.exit()
-        for sample_attribute in root.iter('SAMPLE_ATTRIBUTE'):
+        print(root)
+        print(root.tag)
+        for child in root.iter():
+            #print(child.tag)
+            #print(child.attrib)
+            print(child.text)
+
+
+        print("end of loop")
+        
+        xml_elem = []
+        for sample in root.findall("./SAMPLE/"):
+            xml_elem.append(sample.tag)
+            print(sample.text)
+        
+        
+        title = []
+        for titl in root.findall("./SAMPLE/TITLE"):
+            title.append(titl.tag)
+            title.append(titl.text)
+        
+        
+        links = []
+        for link in root.findall("./SAMPLE/SAMPLE_LINKS/SAMPLE_LINK/XREF_LINK/"):
+            links.append(link.tag)
+            links.append(link.text)
+        
+        attribs = []
+        for attrib in root.findall("./SAMPLE/SAMPLE_ATTRIBUTES/SAMPLE_ATTRIBUTE/"):
+            attribs.append(attrib.tag)
+            attribs.append(attrib.text)
+        
+        print(xml_elem, title, links, attribs)
+        #print(sample_attribute.attrib)
         # Loop all sentence in the xml
             #for opinion in sample_attribute.iter('Opinion'):
             # Loop all Opinion of a particular sentence.
                 #print([opinion.attrib['polarity'], sentence.find('text').text, opinion.attrib['category']])
         # Extract data from XML and store in a dictionary
-            xml_data = {}
-            xml_data['Filename'] = filename  # Example: storing the filename
-            xml_data['Value1'] = root.find('PRIMARY_ID').text  # Example: extracting value from XML element
-            xml_data['Value2'] = root.find('ID').text  # Example: extracting another value
+        #    xml_data = {}
+        #    xml_data['Filename'] = filename  # Example: storing the filename
+        #    xml_data['Value1'] = root.find('PRIMARY_ID').text  # Example: extracting value from XML element
+        #    xml_data['Value2'] = root.find('ID').text  # Example: extracting another value
         
         # Append the dictionary to the data list
+        sys.exit()
         data.append(xml_data)
 
 # Get the list of keys from the first dictionary in the data list
