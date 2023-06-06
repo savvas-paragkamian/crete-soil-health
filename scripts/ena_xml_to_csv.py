a52290d97dd6e2a0adafd7e6e59c423336e4e521
“savvas-paragkamian” <s.paragamian@gmail.com>
2023-06-06 17:43:17 +0300

    almost ready to print

--- scripts/ena_xml_to_csv.py
+++ scripts/ena_xml_to_csv.py
@@ -14,7 +14,7 @@
 import xml.etree.ElementTree as ET
 import csv
 import os,sys
-import pandas as pd
+#import pandas as pd
 
 # Directory containing the XML files
 xml_dir = 'ena_samples_attr/'
@@ -58,23 +58,9 @@ for filename in os.listdir(xml_dir):
         
         tree = ET.parse(os.path.join(xml_dir, filename))
         root = tree.getroot()
-        #pd_df = pd.read_xml(os.path.join(xml_dir, filename))
-        #print(pd_df)
 
-        #print(filename)
-        #print(root)
-        #print(root.tag)
-        # this loop prints all the text of the xml, without any tags and with \n
-        # between.
-        #
-        #for child in root.iter():
-        #    print(child.tag)
-        #    print(child.attrib)
-        #    print(child.text)
-
-        
-        xml_elem = []
         # here we store all the categories of the XML file
+        xml_elem = []
         for sample in root.findall("./SAMPLE/"):
             xml_elem.append(sample.tag)
         
@@ -83,56 +69,46 @@ for filename in os.listdir(xml_dir):
             title.append(titl.tag)
             title.append(titl.text)
         
-
         link_attributes = root.findall("./SAMPLE/SAMPLE_LINKS/SAMPLE_LINK/XREF_LINK/")
         links = groub_xml_elements(link_attributes, "DB")
-        print(links)
-
-
-        for link in root.findall("./SAMPLE/SAMPLE_LINKS/SAMPLE_LINK/XREF_LINK/"):
-            links.append(link.tag + "," + link.text)
-            links.append(link.text)
-        
-        attribs = []
-        for attrib in root.findall("./SAMPLE/SAMPLE_ATTRIBUTES/SAMPLE_ATTRIBUTE/"):
-            attribs.append(attrib.tag)
-            attribs.append(attrib.text)
         
         sample_attr = root.findall("./SAMPLE/SAMPLE_ATTRIBUTES/SAMPLE_ATTRIBUTE/")
         attribs = groub_xml_elements(sample_attr, "TAG")
-    
 
-
-        #print(xml_elem, title, links, attribs)
-        #print(xml_elem)
-        sys.exit()
-        #print(sample_attribute.attrib)
-        # Loop all sentence in the xml
-            #for opinion in sample_attribute.iter('Opinion'):
-            # Loop all Opinion of a particular sentence.
-                #print([opinion.attrib['polarity'], sentence.find('text').text, opinion.attrib['category']])
         # Extract data from XML and store in a dictionary
-        #    xml_data = {}
-        #    xml_data['Filename'] = filename  # Example: storing the filename
-        #    xml_data['Value1'] = root.find('PRIMARY_ID').text  # Example: extracting value from XML element
-        #    xml_data['Value2'] = root.find('ID').text  # Example: extracting another value
+        xml_data = {}
+        xml_data['filename'] = filename  # store the filename
+        xml_data['title'] = title # store the title list
+        xml_data['links'] = links # store the links list
+        xml_data['attributes'] = attribs # store the sample attributes
         
         # Append the dictionary to the data list
-        sys.exit()
         data.append(xml_data)
 
 # Get the list of keys from the first dictionary in the data list
-fieldnames = data[0].keys()
+fieldnames = ['filename', 'TAG', 'VALUE', 'UNITS']
 
 # Write the data to TSV file
 with open(output_file, 'w', newline='') as tsvfile:
-    writer = csv.DictWriter(tsvfile, fieldnames=fieldnames, delimiter='\t')
+    writer = csv.writer(tsvfile, delimiter='\t')
     
     # Write the header
-    writer.writeheader()
+    #writer.writeheader()
     
     # Write the data rows
-    writer.writerows(data)
+    for dictionary in data:
+        
+        for tags in dictionary:
+
+            if tags == 'filename':
+                filename = tags
+
+            if tags == 
+         #   print(tags)
+                for item in dictionary[tags]:
+                    row = files['filename'] + "\t" + tags + "\t" + item + "\n"
+                    print(row)
+        #writer.writerows(data)
 
 print(f"Conversion complete. The TSV file '{output_file}' has been created.")
 
