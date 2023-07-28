5e25e78600804491319475e7721bf74e1d01a4fb
“savvas-paragkamian” <s.paragamian@gmail.com>
2023-07-28 13:23:06 +0300

    hpc job submission

---
+++ scripts/isd_crete_hpc_job_dada2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+
+#SBATCH --partition=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem=100G
+#SBATCH --job-name="isd-species"
+#SBATCH --mail-user=s.paragkamian@hcmr.gr
+#SBATCH -o slurm.%N.%j.out # STDOUT
+#SBATCH -e slurm.%N.%j.err # STDERR
+#SBATCH --requeue
+
+start=`date +%s`
+
+module purge # unloads all previous loads
+
+module load  R/4.1.1 #loads  R/4.1.1
+
+/home1/s.paragkamian/isd-crete/scripts/dada2_taxonomy.R
+
+module purge
+
+end=`date +%s`
+runtime=$((end-start))
+echo "Job ID: " $SLURM_JOB_ID
+echo "Job name: " $SLURM_JOB_NAME
+echo $runtime "in seconds" 
+echo $((runtime/60)) "in minutes" 
