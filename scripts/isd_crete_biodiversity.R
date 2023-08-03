0b22e7910e2bcfda57ce219d372f1bb74251c918
savvas-paragkamian <s.paragkamian@gmail.com>
2023-08-03 16:01:44 +0300

    asv total descriptives

--- scripts/isd_crete_biodiversity.R
+++ scripts/isd_crete_biodiversity.R
@@ -62,18 +62,45 @@ taxa_asv_l_filter$classification <- sapply(taxa_asv_l_filter$higherClassificatio
 
 # merge abundance and taxonomy
 
-crete_biodiversity <- abundance_asv_l %>%
-    left_join(taxa_asv_l_filter,by=c("asv"="asv"))
+crete_biodiversity_all <- abundance_asv_l %>%
+    left_join(taxa_asv_l_filter,by=c("asv"="asv")) %>%
+    filter(abundance>0)
 
+### remove the asvs that don't have a taxonomy
+crete_biodiversity <- crete_biodiversity_all %>%
+    filter(!is.na(classification)) 
 
+write_delim(crete_biodiversity,"../results/crete_biodiversity_asv.tsv",delim="\t")
 # Descriptives
-table(taxa_asv_l$Classification_levels)
-table(taxa_asv_l$Classification_levels, taxa_asv_l$higherClassification)
+
+## total ASVs and taxonomy
+asv_no_taxonomy <- crete_biodiversity_all %>% 
+    distinct(asv,classification) %>%
+    filter(is.na(classification))
+
+print(paste0("ASVs without taxonomy: ", nrow(asv_no_taxonomy)))
+
+## total ASV summary
+crete_biodiversity_asv <- crete_biodiversity %>% 
+    group_by(asv) %>% 
+    summarise(n_samples=n(),
+              total_abundance=sum(abundance)) %>%
+    ungroup()
+
+asv_sample_dist <- crete_biodiversity_asv %>%
+    group_by(n_samples) %>%
+    summarise(n_asv=n(), sum_abundance=sum())
+
+write_delim(asv_sample_dist,"../results/asv_sample_dist.tsv",delim="\t")
 
 ## taxonomic diversity per sample
 
 crete_biodiversity_s <- crete_biodiversity %>% 
-    filter(abundance>10) %>%
+    filter(abundance>10, !is.na(classification)) %>%
     group_by(sampleID, classification) %>% 
     summarise(n_taxa=n(), .groups="keep")
+## create bar plots for each sample at family level, class level, Phylum etc
 
+## shannon per sample
+## bray curtis per sample
+## 
