587a105d6c7231707a108861cd82ebbc49cf338f
savvas-paragkamian <s.paragkamian@gmail.com>
2023-08-05 12:11:38 +0300

    figure 1 with 2 maps, crete base and corine land cover ready

--- scripts/figures.R
+++ scripts/figures.R
@@ -27,6 +27,11 @@ crete_peaks <- read_delim("../spatial_data/crete_mountain_peaks.csv", delim=";",
              remove=F,
              crs="WGS84")
 
+clc_crete_shp <- st_read("../spatial_data/clc_crete_shp/clc_crete_shp.shp")
+natura_crete <- sf::st_read("../spatial_data/natura2000/natura2000_crete.shp")
+wdpa_crete <- sf::st_read("../spatial_data/wdpa_crete/wdpa_crete.shp")
+natura_crete_land <- st_intersection(natura_crete, crete_shp)
+natura_crete_land_sci <- natura_crete_land %>% filter(SITETYPE=="B")
 # raster DEM hangling
 dem_crete <- raster("../spatial_data/dem_crete/dem_crete.tif")
 dem_crete_pixel <- as(dem_crete, "SpatialPixelsDataFrame")
@@ -76,7 +81,7 @@ crete_base <- ggplot() +
           legend.box.background = element_blank())
 
 
-ggsave("../figures/fig1.tiff",
+ggsave("../figures/fig1a.tiff",
        plot=crete_base,
        height = 10,
        width = 20,
@@ -84,7 +89,7 @@ ggsave("../figures/fig1.tiff",
        units="cm",
        device="tiff")
 
-ggsave("../figures/fig1.png",
+ggsave("../figures/fig1a.png",
        plot=crete_base,
        height = 10,
        width = 20,
@@ -92,6 +97,109 @@ ggsave("../figures/fig1.png",
        units="cm",
        device="png")
 
+## Crete Corine
+
+crete_corine <- ggplot() +
+    geom_sf(crete_shp, mapping=aes()) +
+    geom_sf(clc_crete_shp,
+            mapping=aes(fill=LABEL1),
+            alpha=1,
+            colour="transparent",
+            show.legend=T) +
+    geom_sf(natura_crete_land_sci,
+            mapping=aes(color="Natura2000 HSD"),
+            linewidth=0.6,
+            fill=NA,
+            alpha=1,
+            show.legend=T) +
+#    geom_sf(crete_peaks,
+#            mapping=aes(),
+#            color = "#D55E00",
+#            size=1,
+#            alpha=1,
+#            show.legend=F) +
+#    geom_label(data = crete_peaks, 
+#               mapping=aes(x = X, y = Y, label = name),
+#               size = 1.5,
+#               nudge_x = 0.05,
+#               nudge_y=0.05, label.padding = unit(0.1, "lines"))+ 
+    scale_fill_manual(values = c("Artificial surfaces"="#000000",
+                                 "Agricultural areas"="#E69F00",
+                                 "Forest and semi natural areas" = "#009E73",
+                                 "Water bodies" = "#0072B2",
+                                 "Natura2000 HSD"=NA),
+                      guide = "legend") +
+    scale_colour_manual(values = c("Natura2000 HSD" = "#56B4E9"),
+                        guide = "legend") +
+    guides(fill = guide_legend(override.aes = list(color = "transparent", alpha=1) ),
+           colour = guide_legend(override.aes = list(alpha=1, fill="transparent") ) )+
+    coord_sf(crs="WGS84") +
+    theme_bw()+
+    theme(axis.title=element_blank(),
+          axis.text=element_text(colour="black"),
+          legend.title = element_blank(),
+          legend.position = "bottom",
+          legend.box.background = element_blank(),
+          legend.key.size = unit(6, "mm"), 
+          legend.text=element_text(size=8))
+
+ggsave("../figures/Fig1b.tiff", 
+       plot=crete_corine, 
+       height = 10, 
+       width = 20,
+       dpi = 600, 
+       units="cm",
+       device="tiff")
+
+ggsave("../figures/Fig1b.png", 
+       plot=crete_corine, 
+       height = 10, 
+       width = 20,
+       dpi = 600, 
+       units="cm",
+       device="png")
+
+
+fig1 <- ggarrange(crete_base,crete_corine,
+          labels = c("A", "B"),
+          align = "hv",
+          widths = c(1,0.6),
+          ncol = 1,
+          nrow = 2,
+          font.label=list(color="black",size=22),
+          legend="bottom") + bgcolor("white")
+
+ggsave("../figures/Fig1.tiff", 
+       plot=fig1, 
+       height = 40, 
+       width = 30,
+       dpi = 600, 
+       units="cm",
+       device="tiff")
+
+ggsave("../figures/Fig1.png", 
+       plot=fig1, 
+       height = 40, 
+       width = 30,
+       dpi = 600, 
+       units="cm",
+       device="png")
+
+ggsave("../figures/Fig1.pdf", 
+       plot=fig1, 
+       height = 40, 
+       width = 30,
+       dpi = 600, 
+       units="cm",
+       device="pdf")
+
+ggsave("../figures/Fig1-small.png", 
+       plot=fig1, 
+       height = 40, 
+       width = 30,
+       dpi = 300, 
+       units="cm",
+       device="png")
 # Biodiversity statistics
 
 ## ASVs
