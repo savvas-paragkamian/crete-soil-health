5bc20474f6ca47f67f73eea400a66789f60bf201
savvas-paragkamian <s.paragkamian@gmail.com>
2023-08-27 10:52:49 +0300

    hpc jobs scripts

---
+++ scripts/isd_crete_pema_asv.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+
+#SBATCH --partition=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem=100G
+#SBATCH --job-name="isd-2016-asv-pema"
+#SBATCH --mail-user=s.paragkamian@hcmr.gr
+#SBATCH -o slurm.%N.%j.out # STDOUT
+#SBATCH -e slurm.%N.%j.err # STDERR
+#SBATCH --requeue
+
+start=`date +%s`
+
+module purge # unloads all previous loads
+
+module load singularity/3.7.1 #loads singularity
+
+singularity run -B /home1/s.paragkamian/isd-crete/pema_asv/:/mnt/analysis /mnt/big/containers/singularity/pema_v.2.1.4.sif
+
+module unload singularity/3.7.1 #unloads singularity
+
+end=`date +%s`
+runtime=$((end-start))
+echo "Job ID: " $SLURM_JOB_ID
+echo "Job name: " $SLURM_JOB_NAME
+echo $runtime "in seconds" 
+echo $((runtime/60)) "in minutes" 
