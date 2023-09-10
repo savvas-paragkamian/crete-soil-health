a0ecd43606a09b849b95b5e71392843ec28c09e3
savvas-paragkamian <s.paragkamian@gmail.com>
2023-09-10 06:19:28 +0300

    spatial CLC add to metadata

--- scripts/isd_crete_spatial.R
+++ scripts/isd_crete_spatial.R
@@ -21,11 +21,14 @@ library(tibble)
 library(readr)
 library(magrittr)
 library(tidyr)
+library(ggnewscale)
+library(ggpubr)
+library(jpeg)
+library(raster)
+library(scales)
 
 
 # Metadata
-
-master_metadata_old <- read.delim("Crete/Composite_MetaData_from_master.csv", sep=",")
 
 metadata_long <- read_delim("ena_metadata/ena_isd_2016_attributes.tsv", delim="\t") %>%
     mutate(VALUE=gsub("\\r(?!\\n)","", VALUE, perl=T))
@@ -47,10 +50,89 @@ metadata_wide$longitude <- as.numeric(metadata_wide$`geographic_location_(longit
 metadata_wide$elevation <- as.numeric(metadata_wide$`geographic_location_(elevation)`)
 metadata_wide$amount_or_size_of_sample_collected <- as.numeric(metadata_wide$amount_or_size_of_sample_collected)
 
-metadata <- metadata_wide %>%
-    select(`ENA-RUN`, source_material_identifiers, total_nitrogen, water_content,total_organic_carbon,sample_volume_or_weight_for_DNA_extraction,DNA_concentration,latitude, longitude,elevation, amount_or_size_of_sample_collected, vegetation_zone) %>%
+locations_spatial <- metadata_wide %>%
+    dplyr::select(`ENA-RUN`, source_material_identifiers, total_nitrogen, water_content,total_organic_carbon,sample_volume_or_weight_for_DNA_extraction,DNA_concentration,latitude, longitude,elevation, amount_or_size_of_sample_collected, vegetation_zone) %>%
     arrange(`ENA-RUN`) %>%
-    as.data.frame()
+    mutate(route = sub("isd_(.*.)_site.*" ,"\\1" ,metadata$source_material_identifiers)) %>%
+    st_as_sf(coords=c("longitude", "latitude"),
+             remove=F,
+             crs="WGS84")
 
-rownames(metadata) <- metadata$`ENA-RUN`
-metadata <- metadata[,-1]
+# spatial data
+crete_shp <- sf::st_read("spatial_data/crete/crete.shp")
+crete_peaks <- read_delim("spatial_data/crete_mountain_peaks.csv", delim=";", col_names=T) %>%
+    st_as_sf(coords=c("X", "Y"),
+             remove=F,
+             crs="WGS84")
+
+clc_crete_shp <- st_read("spatial_data/clc_crete_shp/clc_crete_shp.shp")
+natura_crete <- sf::st_read("spatial_data/natura2000/natura2000_crete.shp")
+wdpa_crete <- sf::st_read("spatial_data/wdpa_crete/wdpa_crete.shp")
+natura_crete_land <- st_intersection(natura_crete, crete_shp)
+natura_crete_land_sci <- natura_crete_land %>% filter(SITETYPE=="B")
+# raster DEM hangling
+dem_crete <- raster("spatial_data/dem_crete/dem_crete.tif")
+dem_crete_pixel <- as(dem_crete, "SpatialPixelsDataFrame")
+dem_crete_df <- as.data.frame(dem_crete_pixel) %>% filter(dem_crete>0)
+
+
+
+# Colorblind palette
+palette.colors(palette = "Okabe-Ito")
+# Crete figures
+cols=c("chocolate1","cornflowerblue","darkgoldenrod1", "darkolivegreen4", "darkorchid1", "goldenrod3", "palevioletred3", "peachpuff4", "turquoise","skyblue")
+
+crete_base <- ggplot() +
+    geom_sf(crete_shp, mapping=aes()) +
+    geom_raster(dem_crete_df, mapping=aes(x=x, y=y, fill=dem_crete))+
+    geom_point(locations_spatial,
+            mapping=aes(x=longitude, y=latitude, color=route),
+            size=1,
+            alpha=0.8,
+            show.legend=T) +
+#    geom_sf(crete_peaks,
+#            mapping=aes(),
+#            colour=na,
+#            size=1,
+#            alpha=1,
+#            show.legend=f) +
+    geom_label(data = crete_peaks,
+               mapping=aes(x = X, y = Y, label = name),
+               size = 1.8,
+               nudge_x = 0.05,
+               nudge_y=0.05,
+               label.padding = unit(0.1, "lines"))+
+    scale_fill_gradientn(guide = guide_colourbar(barwidth = 0.5, barheight = 3.5,
+                                  title="elevation",
+                                  direction = "vertical",
+                                  title.vjust = 0.8),
+                        colours = c("snow3","#f0e442","#d55e00","#cc79a7"),
+                        breaks = c(100, 800, 1500, 2400),
+                        labels = c(100, 800, 1500, 2400))+
+    scale_color_manual(values=cols, guide=guide_legend(nrow=4))+
+    coord_sf(crs="wgs84") +
+    theme_bw()+
+    theme(axis.title=element_blank(),
+          axis.text=element_text(colour="black"),
+          legend.text=element_text(size=8),
+          legend.title = element_text(size=8),
+          legend.position = "bottom",
+          legend.box.background = element_blank())
+
+
+ggsave("figures/fig1a.tiff",
+       plot=crete_base,
+       height = 10,
+       width = 20,
+       dpi = 300,
+       units="cm",
+       device="tiff")
+
+ggsave("figures/fig1a.png",
+       plot=crete_base,
+       height = 10,
+       width = 20,
+       dpi = 300,
+       units="cm",
+       device="png")
+
