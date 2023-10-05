f2c94de6b135643386a84ac68537a1c8c41a59e5
savvas-paragkamian <s.paragkamian@gmail.com>
2023-10-05 16:31:01 +0300

    remove parts to different scripts

--- scripts/isd_crete_numerical_ecology.R
+++ scripts/isd_crete_numerical_ecology.R
@@ -28,25 +28,18 @@ library(ggplot2)
 
 ################################## Load data ##################################
 crete_biodiversity <- read_delim("results/crete_biodiversity_asv.tsv",delim="\t")
+asv_metadata <- read_delim("results/asv_metadata.tsv",delim="\t")
 
-crete_biodiversity_matrix <- readRDS("results/crete_biodiversity_matrix.RDS")
+#crete_biodiversity_matrix <- readRDS("results/crete_biodiversity_matrix.RDS")
+biodiversity_srs <- readRDS("results/biodiversity_srs.RDS")
 tax_tab <- readRDS("results/tax_tab.RDS")
 
 # Metadata
 
 master_metadata_old <- read.delim("Crete/Composite_MetaData_from_master.csv", sep=",")
 
-metadata <- read_delim("results/metadata_all.tsv", delim="\t")
+metadata <- read_delim("results/sample_metadata.tsv", delim="\t")
 
-## location pairs of each site
-metadata$sites <- gsub("_loc_*.","", metadata$source_material_identifiers)
-metadata$location <- gsub(".*(loc_.*)","\\1", metadata$source_material_identifiers)
-
-samples_locations <- metadata %>%
-    dplyr::select(ENA_RUN,sites,location) %>%
-    pivot_wider(id_cols=sites,
-                names_from=location,
-                values_from=ENA_RUN)
 
 # differences of old and new data
 
@@ -66,13 +59,13 @@ dist_long <- function(x,method){
 
 
 ####################### Destriptors Statistics ###############################
-pw <- pairwise.wilcox.test(metadata_all$Observed, metadata_all$vegetation_zone, p.adjust.method="BH")
-pw_e <- pairwise.wilcox.test(metadata_all$Observed, metadata_all$elevation_bin, p.adjust.method="BH")
-pw_s <- pairwise.wilcox.test(metadata_all$Shannon, metadata_all$LABEL1, p.adjust.method="BH")
+pw <- pairwise.wilcox.test(metadata$Observed, metadata_all$vegetation_zone, p.adjust.method="BH")
+pw_e <- pairwise.wilcox.test(metadata$Observed, metadata_all$elevation_bin, p.adjust.method="BH")
+pw_s <- pairwise.wilcox.test(metadata$Shannon, metadata_all$LABEL1, p.adjust.method="BH")
 print(pw)
 
 
-box_shannon <- ggplot(data=metadata_all, mapping=aes(x=LABEL2, y=Shannon))+
+box_shannon <- ggplot(data=metadata, mapping=aes(x=LABEL2, y=Shannon))+
     geom_boxplot()+
     geom_jitter(width = 0.2)+
     theme_bw()+
@@ -81,7 +74,6 @@ box_shannon <- ggplot(data=metadata_all, mapping=aes(x=LABEL2, y=Shannon))+
                                      angle = 45,
                                      vjust = 1,
                                      hjust=1))
-
 
 ggsave("figures/box_shannon.png", 
        plot=box_shannon, 
@@ -92,11 +84,11 @@ ggsave("figures/box_shannon.png",
 
 
 # correlations of diversity and other numerical metadata
-with(metadata_all, cor(Shannon, water_content))
+with(metadata, cor(Shannon, water_content))
 
 
-metadata_n <- metadata_all
-rownames(metadata_n) <- metadata_all$ENA_RUN
+metadata_n <- metadata
+rownames(metadata_n) <- metadata$ENA_RUN
 nums <- unlist(lapply(metadata_n, is.numeric), use.names = FALSE)
 metadata_n <- metadata_n[,c(nums)]
 cc <- cor(metadata_n)
@@ -200,124 +192,3 @@ nmds <- vegan::metaMDS(t(crete_biodiversity_matrix),
 stressplot(nmds)
 ordiplot(nmds,display="sites", cex=1.25)
 
-# ancombc analysis
-
-assays = SimpleList(counts = crete_biodiversity_matrix)
-smd = DataFrame(metadata1)
-tax_tab = DataFrame(tax_tab)
-
-## create TSE
-tse = TreeSummarizedExperiment(assays = assays,
-                               colData = smd,
-                               rowData = tax_tab)
-## convert TSE to phyloseq
-pseq = makePhyloseqFromTreeSummarizedExperiment(tse)
-
-total = median(sample_sums(pseq))
-standf = function(x, t=total) round(t * (x / sum(x)))
-
-pseq_std  = transform_sample_counts(pseq,standf )
-gpsf = filter_taxa(pseq_std, function(x) sd(x)/mean(x) > 3.0, TRUE)
-
-gpsfb = subset_taxa(gpsf, Order=="Frankiales")
-
-jpeg(file="saving_plot1.jpeg")
-plot_bar(gpsfb, "LABEL1", "Abundance", "Family", title="test")
-dev.off()
-
-
-GP = pseq_std
-#wh0 = genefilter_sample(GP, filterfun_sample(function(x) x > 5), A=0.5*nsamples(GP))
-#GP1 = prune_taxa(wh0, GP)
-
-#GP1 = transform_sample_counts(GP, function(x) 1E6 * x/sum(x))
-
-GP.ord <- ordinate(pseq, "NMDS", "bray")
-
-jpeg(file="saving_plot1.jpeg")
-p1 = plot_ordination(GP, GP.ord, type="samples", color="LABEL1", title="taxa")
-p1
-dev.off()
-
-
-###
-set.seed(123)
-output = ancombc2(data = tse, assay_name = "counts", tax_level = "Family",
-                  fix_formula = NULL, rand_formula = NULL,
-                  p_adj_method = "holm", 
-                  prv_cut = 0.10, lib_cut = 100, s0_perc = 0.05,
-                  group = NULL, struc_zero = FALSE, neg_lb = FALSE,
-                  alpha = 0.05, n_cl = 2, verbose = TRUE,
-                  global = FALSE, pairwise = FALSE, 
-                  dunnet = FALSE, trend = FALSE,
-                  iter_control = list(tol = 1e-5, max_iter = 20, 
-                                      verbose = FALSE),
-                  em_control = list(tol = 1e-5, max_iter = 100),
-                  lme_control = NULL, 
-                  mdfdr_control = list(fwer_ctrl_method = "holm", B = 100), 
-                  trend_control = NULL)
-
-data(dietswap, package = "microbiome")
-    tse = mia::makeTreeSummarizedExperimentFromPhyloseq(dietswap)
-    colData(tse)$bmi_group = factor(colData(tse)$bmi_group,
-                                    levels = c("obese",
-                                               "overweight",
-                                               "lean"))
-set.seed(123)
-# Note that setting max_iter = 1 and B = 1 is only for the sake of speed
-# Use default or larger values for max_iter and B for better performance
-out = ancombc2(data = tse, assay_name = "counts", tax_level = "Phylum",
-               fix_formula = "nationality + timepoint + bmi_group",
-               rand_formula = "(timepoint | subject)",
-               p_adj_method = "holm",
-               prv_cut = 0.10, lib_cut = 1000, s0_perc = 0.05,
-               group = "bmi_group", struc_zero = TRUE, neg_lb = TRUE,
-               alpha = 0.05, n_cl = 1, verbose = TRUE, global = TRUE, pairwise = TRUE, dunnet = TRUE, trend = TRUE,
-               iter_control = list(tol = 1e-2, max_iter = 1, verbose = TRUE),
-               em_control = list(tol = 1e-5, max_iter = 1),
-               lme_control = lme4::lmerControl(),
-               mdfdr_control = list(fwer_ctrl_method = "holm", B = 1),
-               trend_control = list(contrast =list(matrix(c(1, 0, -1, 1),nrow = 2, byrow = TRUE)),node = list(2), solver = "ECOS", B = 1))
-res_prim = out$res
-res_global = out$res_global
-res_pair = out$res_pair
-res_dunn = out$res_dunn
-res_trend = out$res_trend
-
-
-
-
-
-### highest species biodiversity sample
-print("sample with the highest microbial species diversity")
-crete_biodiversity_s[which(crete_biodiversity_s$Species==max(crete_biodiversity_s$Species)),]
-
-highest_crete_biodiversity_s <- crete_biodiversity_s %>% 
-    left_join(metadata_wide,
-              by=c("ENA-RUN"="ENA-RUN")) %>% 
-    select(place_name,source_material_identifiers, Species, `ENA-RUN`)
-
-write_delim(highest_crete_biodiversity_s,
-            "results/highest_crete_biodiversity_sample_taxonomy.tsv",
-            delim="\t")
-
-################################# Taxonomy #####################################
-## Phyla distribution
-
-phyla_dist <- crete_biodiversity %>%
-    group_by(sampleID,Phylum) %>%
-    summarise(sum_abundance=sum(abundance), .groups="keep") %>%
-    na.omit(Phylum)
-
-total_phyla_dist <- phyla_dist %>% 
-    group_by(Phylum) %>%
-    summarise(sum_abundance=sum(sum_abundance),
-              n_samples=n()) %>%
-    mutate(sampleID="All samples") %>%
-    arrange(desc(n_samples))
-
-
-## create bar plots for each sample at family level, class level, Phylum etc
-## shannon per sample
-## bray curtis per sample
-## 
