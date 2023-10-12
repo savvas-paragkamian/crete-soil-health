0a8b3aa0969128e729c01de0a71ba4bd3aec12db
savvas-paragkamian <s.paragkamian@gmail.com>
2023-10-12 15:38:07 +0300

    umap with multiple exports

--- scripts/isd_crete_umap.py
+++ scripts/isd_crete_umap.py
@@ -16,50 +16,55 @@ import numpy as np
 import os, sys
 import umap
 
+## files
 community_matrix_path = "results/genera_samples_matrix.tsv"
-samples_umap_file = "results/umap_samples.tsv"
-taxa_umap_file = "results/umap_taxa.tsv"
+output_path = "results/"
 
 community_matrix = pd.read_csv(community_matrix_path, sep="\t")
 community_array = community_matrix.drop(columns=['ENA_RUN'])
+
 # taxa, the colnames to a new dataframe
 cols_list = community_array.columns.tolist()
 df_taxa = pd.DataFrame(cols_list)
-#print(df_taxa)
 # samples
 df_samples = community_matrix[['ENA_RUN']]
-#print(df_samples)
 community_array = community_array.to_numpy()
-#print(community_array.shape)
 
 # for taxa UMAP the array needs to be transposed 
 community_array_t = np.transpose(community_array)
-#print(community_array_t.shape)
 
-def umap_function(array, df_ids):
+def umap_function(array, df_ids, n_components,axis):
     
     np.random.seed(123)
     # umap function
     embedding = umap.UMAP(n_neighbors=80,
                           min_dist=0.3,
-                          n_components=3,
+                          n_components=n_components,
                           metric='braycurtis').fit_transform(array)
     
     #add NumPy matrix as new columns in DataFrame
     df = pd.concat([df_ids, pd.DataFrame(embedding)], axis=1)
-    df.columns =['id', 'UMAP1', 'UMAP2', 'UMAP3']
+    # add colnames
+    colnames =['id']
+    for i in range(1,n_components+1):
+        
+        colname = "UMAP" + str(i)
+        colnames.append(colname)
 
-    return(df)
+    df.columns = colnames
+    # create the tsv file
+    output_file = output_path + "umap_" + axis + "_" + str(n_components) + ".tsv"
+    df.to_csv(output_file, header=True, index=None, sep='\t')
+
+    #return(df)
 
 # samples
 print("UMAP samples is ongoing")
-samples_umap = umap_function(community_array,df_samples)
-# save to file
-samples_umap.to_csv(samples_umap_file, header=True, index=None, sep='\t')
+umap_function(community_array,df_samples, 3, "samples")
+umap_function(community_array,df_samples, 2, "samples")
 
 # taxa
 print("UMAP taxa is ongoing")
-taxa_umap = umap_function(community_array_t,df_taxa)
-# save to file
-taxa_umap.to_csv(taxa_umap_file, header=True, index=None, sep='\t')
+umap_function(community_array_t,df_taxa,3,"genera")
+umap_function(community_array_t,df_taxa,2,"genera")
 
