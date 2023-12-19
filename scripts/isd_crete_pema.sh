7e3659c6d89716cddd801252ced8982c694181c7
savvas-paragkamian <s.paragkamian@gmail.com>
2023-12-19 23:40:13 +0200

    pema 215 version

--- scripts/isd_crete_pema.sh
+++ scripts/isd_crete_pema.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=20
 #SBATCH --mem=100G
-#SBATCH --job-name="isd-2016-asv-pema"
+#SBATCH --job-name="pema215_true_fastp"
 #SBATCH --mail-user=s.paragkamian@hcmr.gr
 #SBATCH -o slurm.%N.%j.out # STDOUT
 #SBATCH -e slurm.%N.%j.err # STDERR
@@ -16,13 +16,13 @@ module purge # unloads all previous loads
 
 module load singularity/3.7.1 #loads singularity
 
-#singularity run -B /home1/s.paragkamian/isd-crete/pema_test_asv/:/mnt/analysis /mnt/big/containers/singularity/pema_v.2.1.4.sif
+singularity run -B /home1/s.paragkamian/isd-crete/pema215_test2/:/mnt/analysis /home1/s.paragkamian/pema_v2.1.5.sif
 
 ### for checkpoints
-singularity exec \
-    -B /home1/s.paragkamian/isd-crete/pema_asv/:/mnt/analysis \
-    /mnt/big/containers/singularity/pema_v.2.1.4.sif \
-    /home/tools/BDS/.bds/bds -r /mnt/analysis/clustering.chp
+#singularity exec \
+#    -B /home1/s.paragkamian/isd-crete/pema_asv/:/mnt/analysis \
+#    /mnt/big/containers/singularity/pema_v.2.1.4.sif \
+#    /home/tools/BDS/.bds/bds -r /mnt/analysis/clustering.chp
 
 module unload singularity/3.7.1 #unloads singularity
 
